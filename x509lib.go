a4d639bc535f032374dac21c89b11303c0213c1b
Doowon Kim <doowon@Doowons-MacBook-Pro.local>
2019-06-12 20:12:59 -0700

    init

---
+++ x509lib.go
@@ -0,0 +1,101 @@
+package x509lib
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+// LoadPEMCertWithPath loads a certificate and
+// returns a x509 stucture when path is give as parameter
+func LoadPEMCertWithPath(path string) (*x509.Certificate, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	bytesBlock, _ := pem.Decode(bytes)
+	if bytesBlock == nil {
+		fmt.Println("ERROR: failed to parse certificate PEM")
+		return nil, errors.New("ERROR: failed to parse certificate PEM")
+	}
+	cert, err := x509.ParseCertificate(bytesBlock.Bytes)
+	if err != nil {
+		fmt.Println("ERROR:", path, "failed to parse certificate: "+err.Error())
+		return nil, err
+	}
+	return cert, nil
+}
+
+// LoadDERCertWithPath loads a certificate and
+// returns a x509 stucture when path is give as parameter
+func LoadDERCertWithPath(path string) (*x509.Certificate, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	cert, err := x509.ParseCertificate(bytes)
+	if err != nil {
+		fmt.Println("ERROR:", path, "failed to parse certificate: "+err.Error())
+		return nil, err
+	}
+	return cert, nil
+}
+
+// LoadCertWithPEMBytes loads a certificate and
+// returns a x509 stucture when path is give as parameter
+func LoadCertWithPEMBytes(bytes []byte) (*x509.Certificate, error) {
+	bytesBlock, _ := pem.Decode(bytes)
+	if bytesBlock == nil {
+		fmt.Println("ERROR: failed to parse certificate PEM")
+		return nil, errors.New("ERROR: failed to parse certificate PEM")
+	}
+	cert, err := x509.ParseCertificate(bytesBlock.Bytes)
+	if err != nil {
+		fmt.Println("ERROR: failed to parse certificate: " + err.Error())
+		return nil, err
+	}
+	return cert, nil
+}
+
+// LoadCertWithDERBytes loads a certificate and
+// returns a x509 stucture when path is give as parameter
+func LoadCertWithDERBytes(bytes []byte) (*x509.Certificate, error) {
+	cert, err := x509.ParseCertificate(bytes)
+	if err != nil {
+		fmt.Println("ERROR: failed to parse certificate: " + err.Error())
+		return nil, err
+	}
+	return cert, nil
+}
+
+// IsLeafCert checks if a leaf certificate is
+func IsLeafCert(cert *x509.Certificate) bool {
+	if cert.BasicConstraintsValid && !cert.IsCA {
+		return true
+	}
+	return false
+}
+
+// IsCodeSigningCert checks if code signing cert is
+func IsCodeSigningCert(cert *x509.Certificate) bool {
+	for _, key := range cert.ExtKeyUsage {
+		if key == x509.ExtKeyUsageCodeSigning {
+			return true
+		}
+	}
+	return false
+}
+
+// IsTimeStampingCert checks if the cert is for timestamp
+func IsTimeStampingCert(cert *x509.Certificate) bool {
+	for _, key := range cert.ExtKeyUsage {
+		if key == x509.ExtKeyUsageTimeStamping {
+			return true
+		}
+	}
+	return false
+}
